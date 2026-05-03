98acf4e5ed42bc2454de9ed6832612dc8edf45bb
GitHub <action@github.com>
2026-05-03 09:24:07 +0000

    fix(symfony): register core SDK services in DI and boot global state

--- src/DependencyInjection/InfilePhpExtension.php
+++ src/DependencyInjection/InfilePhpExtension.php
@@ -37,10 +37,47 @@ final class InfilePhpExtension extends Extension
         );
 
         $loader->load('services.yaml');
+
+        // Register core SDK services
+        $container->register(\InfilePhp\Core\FelConfig::class, \InfilePhp\Core\FelConfig::class)
+            ->setFactory([self::class, 'createFelConfig'])
+            ->setArguments([$config])
+            ->setPublic(true);
+
+        $container->register(\InfilePhp\Core\Http\InfileClient::class, \InfilePhp\Core\Http\InfileClient::class)
+            ->setArguments([new \Symfony\Component\DependencyInjection\Reference(\InfilePhp\Core\FelConfig::class)])
+            ->setPublic(true);
     }
 
     public function getAlias(): string
     {
         return 'infile_php';
+    }
+
+    /**
+     * @param array<string, mixed> $config
+     */
+    public static function createFelConfig(array $config): \InfilePhp\Core\FelConfig
+    {
+        return new \InfilePhp\Core\FelConfig(
+            nit: $config['nit'] ?? '',
+            signUser: $config['credentials']['sign_user'] ?? '',
+            signKey: $config['credentials']['sign_key'] ?? '',
+            apiUser: $config['credentials']['api_user'] ?? '',
+            apiKey: $config['credentials']['api_key'] ?? '',
+            environment: \InfilePhp\Core\Enums\Environment::from($config['environment'] ?? 'sandbox'),
+            flow: \InfilePhp\Core\Enums\Flow::from($config['flow'] ?? 'unified'),
+            emailCopy: $config['email_copy'] ?? '',
+            retryTimes: $config['retry']['times'] ?? 3,
+            retrySleep: $config['retry']['sleep'] ?? 2,
+            fallbackEnabled: $config['fallback']['enabled'] ?? true,
+            endpointSign: $config['endpoints']['sign'] ?? 'https://signer-emisores.feel.com.gt/sign_solicitud_firmas/firma_xml',
+            endpointCertify: $config['endpoints']['certify'] ?? 'https://certificador.feel.com.gt/fel/certificacion/v2/dte/',
+            endpointCancel: $config['endpoints']['cancel'] ?? 'https://certificador.feel.com.gt/fel/anulacion/v2/dte/',
+            endpointUnified: $config['endpoints']['unified'] ?? 'https://certificador.feel.com.gt/fel/procesounificado/transaccion/v2/xml',
+            endpointNit: $config['endpoints']['nit'] ?? 'https://consultareceptores.feel.com.gt/rest/action',
+            endpointCui: $config['endpoints']['cui'] ?? 'https://certificador.feel.com.gt/api/v2/servicios/externos/cui',
+            endpointCuiAuth: $config['endpoints']['cui_auth'] ?? 'https://certificador.feel.com.gt/api/v2/servicios/externos/login',
+        );
     }
 }
--- src/InfilePhpBundle.php
+++ src/InfilePhpBundle.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 namespace InfilePhp\Symfony;
 
+use InfilePhp\Core\FelConfig;
+use InfilePhp\Core\InfilePhp;
 use InfilePhp\Symfony\DependencyInjection\InfilePhpExtension;
+use Psr\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
@@ -25,5 +28,16 @@ final class InfilePhpBundle extends Bundle
     public function build(ContainerBuilder $container): void
     {
         parent::build($container);
+    }
+
+    public function boot(): void
+    {
+        /** @var FelConfig $config */
+        $config = $this->container->get(FelConfig::class);
+
+        /** @var EventDispatcherInterface $dispatcher */
+        $dispatcher = $this->container->get('event_dispatcher');
+
+        InfilePhp::configure($config, $dispatcher);
     }
 }
